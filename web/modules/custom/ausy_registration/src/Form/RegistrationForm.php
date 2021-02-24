0a5adf9201b1e9c486ba3ae86b88eb5649a50513
ygstand <ygstand77@gmail.com>
2021-02-24 11:27:25 +0300

    registration form; registration content type

---
+++ web/modules/custom/ausy_registration/src/Form/RegistrationForm.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\ausy_registration\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class RegistrationForm.
+ */
+class RegistrationForm extends FormBase {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Drupal\Core\Routing\CurrentRouteMatch definition.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    $instance->currentRouteMatch = $container->get('current_route_match');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'registration_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name of the employee'),
+      '#maxlength' => 255,
+      '#default_value' => '',
+      '#required' => TRUE,
+    ];
+    $form['amount_of_kids'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Amount of kids'),
+      '#min' => 0,
+      '#required' => TRUE,
+    ];
+    $form['amount_of_vegetarians'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Amount of vegetarians'),
+      '#min' => 0,
+      '#required' => TRUE,
+    ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('E-mail'),
+      '#default_value' => '',
+      '#required' => TRUE,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      // @TODO: Validate fields.
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Display result.
+    foreach ($form_state->getValues() as $key => $value) {
+      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
+    }
+  }
+
+}
